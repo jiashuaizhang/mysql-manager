88321b5dc1a27afc209c1a5c80a86521675e3bf2
zhangjiashuai <zhangjs231@chinaunicom.cn>
2019-09-05 09:38:08 +0800

    批量提交

--- mysql-manager.cmd
+++ mysql-manager.cmd
@@ -9,12 +9,13 @@ set temp=temp.txt
 mysql -h%host% -P%port% -u%user% -p%password% -e"show databases" > %temp%
 set /a n=0
 for /f "tokens=*" %%i in (%temp%) do (
-	if !n! gtr 0 (
-		set database=%%i
-		mysql -h%host% -P%port% -u%user% -p%password% -e"drop database !database!"
+	if !n! gtr 4 (
+		set ddl=drop database %%i;
+		set batch=!batch!!ddl!
 	) 
 	set /a n+=1
 )
+mysql -h%host% -P%port% -u%user% -p%password% -e"!batch!"
 del %temp%
 echo "all done,congratulations!"
 pause
