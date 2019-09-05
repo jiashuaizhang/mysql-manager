ec5b189d23542eda7f7fde0a36a58565229bb774
zhangjiashuai <zhangjs231@chinaunicom.cn>
2019-09-05 10:44:05 +0800

    添加shell实现

---
+++ mysql-manager.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+host="localhost"
+port="3306"
+user="root"
+password="123456"
+
+databases=$(mysql -h$host -P$port -u$user -p$password -e"show databases" |awk '{if(NR > 1) print $1}')
+for var in ${databases[@]}
+do
+   ddl="drop database "$var";"
+   batch=$batch$ddl
+done
+mysql -h$host -P$port -u$user -p$password -e"$batch"
+echo "all done,congratulations!"
